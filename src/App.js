9132c0fe2c195a581bce5bbb283d4297be06c2aa
Abdelmajid Sabiri <159253779+AbdelmajidSabiri@users.noreply.github.com>
2024-10-12 12:13:59 +0100

    Add useReducer hook for state management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useReducer } from "react";
 import "./style.css"
 
+export const ACTIONS = {
+  ADD_DIGIT : "add-digits",
+  CHOOSE_OPERATION : "choose-operation",
+  CLEAR : "clear",
+  DELETE_DIGIT : "delete-digit",
+  EVALUATE : "evaluate"
+
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT :
+      return {
+        ...state,
+        currectOperand : `${currectOperand || ""}${payload.digit}`
+      } 
+  }
+}
+
+
 function App() {
+  const [{currectOperand, previousOperand, operation},dispatch] = useReducer(reducer,{})
+
+  dispatch({type : ACTIONS.ADD_DIGIT, payload:{digit : 1}})
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand}</div>
+        <div className="current-operand">{currectOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
